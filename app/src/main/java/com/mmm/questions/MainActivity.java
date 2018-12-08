4376453305951ac29ea12e0d49a335a10fb40c30
Mars <32443134+uci-mars@users.noreply.github.com>
2018-12-08 13:04:10 -0800

    Initial commit

---
+++ app/src/main/java/com/mmm/questions/MainActivity.java
@@ -0,0 +1,26 @@
+package com.mmm.questions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+}
