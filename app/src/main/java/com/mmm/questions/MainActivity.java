227f06b912224411268decafd3861110713c75cf
Mars <32443134+uci-mars@users.noreply.github.com>
2018-12-08 13:45:10 -0800

    fixed gradle script

--- app/src/main/java/com/mmm/questions/MainActivity.java
+++ app/src/main/java/com/mmm/questions/MainActivity.java
@@ -1,28 +1,19 @@
 package com.mmm.questions;
 
+// The "wall"
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
     }
 
 
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
-    }
 
-    // uwu im just testing to see if i  still know how to git
+    // uwu im just testing to see if i still know how to git
 }
